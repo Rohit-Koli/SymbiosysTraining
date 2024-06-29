eebbe0da65366feb357b338f6af65f589ddf2762
Rohit Koli <kolirohit1252@gmail.com>
2024-06-29 12:19:47 +0530

    Started Learning

---
+++ src/stringIntro/ImmutableEx.java
@@ -0,0 +1,17 @@
+package stringIntro;
+
+public class ImmutableEx {
+	String name;
+	public ImmutableEx(String name) {
+		// TODO Auto-generated constructor stub
+		this.name=name;
+	}
+	public String print() {
+		return name;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
