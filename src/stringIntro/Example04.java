2143bb04391ad2053aca90d8e160ea634793f261
Rohit Koli <kolirohit1252@gmail.com>
2024-07-03 08:15:38 +0530

    Added file Example06.java

--- src/stringIntro/Example04.java
+++ src/stringIntro/Example04.java
@@ -1,14 +1,17 @@
 package stringIntro;
-
-import java.util.Random;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Example04 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str="ABQYEOP";
-		
-		
+		//Check duplicate elements in String an which character is repeated how many times
+		String str="Strawberry";
+		char[] ch = new char[str.length()];
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(int i=0;i<str.length();i++) {
+			map.put(str.charAt(i),map.getOrDefault(str.charAt(i), 0)+1);
+		}				
+		for(Map.Entry<Character, Integer> m:map.entrySet()) {
+			System.out.println("Charac]ers :"+m.getKey()+" values :"+m.getValue());
+		}
 	}
-
 }
